de244b8c356a6a4015e3bc65b182f62295117abe
wawer <wawer@KIWI>
2024-04-16 21:41:10 +0200

    Add project files.

---
+++ BMICalculator/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace BMICalculator
+{
+    public class BMICalculator
+    {
+        public double Calculate(double weight, byte height)
+        {
+            double h = height / 100.0;
+            return weight / (h*h);
+
+        }
+
+    }
+}
---
+++ BMICalculatorTests/BMICalculatorTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BMICalculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMICalculator.Tests
+{
+    [TestClass()]
+    public class BMICalculatorTests
+    {
+        [TestMethod()]
+        public void CalculateTest()
+        {
+            //Arrange
+            BMICalculator bc = new BMICalculator();
+            //act
+            double bmi = bc.Calculate(80.0, 180);
+
+            double expected = 80.0/((180/100.0)*(180/100.0));
+
+            Assert.AreEqual(expected, bmi);
+        }
+    }
+}
